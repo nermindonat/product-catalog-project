49c79420118e439c47cf1b9b51b13d9c732b2426
Nermin Donat <70170606+nermindonat@users.noreply.github.com>
2022-10-26 17:21:24 +0300

    Header and Form component created.

---
+++ pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from '../components/Form';
+
+function Login() {
+    return (
+        <div className='flex mt-20  items-center justify-center'>
+            <Form />
+        </div>
+    );
+}
+
+export default Login;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
